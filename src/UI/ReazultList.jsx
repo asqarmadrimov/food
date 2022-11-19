d8df38d3973ad9d01c4f723d6b7728eb88eef4ee
asqarmadrimov <madrimov2530@gmail.com>
2022-11-19 21:14:47 +0300

    change

--- src/UI/ReazultList.jsx
+++ src/UI/ReazultList.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
-import ReazultItem from './ReazultItem'
+import React from 'react';
+import ReazultItem from './ReazultItem';
+import Setion1 from './Setion1';
+import Corusel from './Corusel';
 
 const ReazultList = ({ catalogy = [] }) => {
   return (
-    <div className="">
+    <div className="text-start">
+      <Setion1 />
+      <Corusel />
+
       <h1 className='my-2 text-white lead display-3 font-monospace'>Category</h1>
       <ul className='list d-flex justify-content-center flex-wrap m-0 p-0'>
         {catalogy.map((item) => (<ReazultItem item={item} />))}
---
+++ src/UI/Setion1.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Setion1 = () => {
+  return (
+    <div>
+      <div className="text-center my-4 mb-5">
+        <h1 className='text-white font-monospace h2'>Learn faster and easier!!!</h1>
+        <p className='mx-auto text-white lh-sm'>The Culinary Arts program is an international program designed to prepare students for employment and leadership in the culinary industry with invaluable practical experience. Students begin with basic kitchen, knife, and cooking skills and start learning about different cuisines. There are two specialized tracks, Culinary Operations and Baking & Pastry, which offer students the ability to focus on their area of interest.</p>
+        </div>
+    </div>
+  )
+}
+
+export default Setion1
